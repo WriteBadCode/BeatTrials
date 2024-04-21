6fde625987d6d787092f2317d5a28bbabf4d8ab8
WriteBadCode <ryantj2005@gmail.com>
2024-04-20 22:04:46 -0700

    kelvin messed up ;D
    jk ;D

--- Game/RythmGame/NoteMainFrame/main.py
+++ Game/RythmGame/NoteMainFrame/main.py
@@ -2,7 +2,7 @@ from data import Data
 import json
 
 file_path = fr".\Game\RythmGame\NoteMainFrame\load_song.json"
-with open(file_path, 'r') as f:
+with open(file_path, 'r', encoding="utf-8") as f:
     loaded_song = json.loads(f.read())
 
 song = Data(loaded_song)
